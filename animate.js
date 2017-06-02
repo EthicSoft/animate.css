db429c2f3f02bfc5acf080ecc4b290e838ea0ca1
Luca Colombi <luca@ethicsoft.it>
2017-06-02 14:27:49 +0200

    Update animate.js

--- animate.js
+++ animate.js
@@ -1,7 +1,13 @@
+/*
+Author: luca.colombi@ethicsoft.it
+
+Site: http://ethicsoft.it
+*/
+
 $.fn.extend({
 		animateCss:function (animationName,direction=null) {
-			var className='animated ' + animationName;
-			var self=$(this).removeClass(className).addClass(className);
+			var c='animated ' + animationName;
+			var self=$(this).removeClass(c).addClass(c);
 			if(direction=="in"){
 				self.show();
 			}else if(direction=="out"){
